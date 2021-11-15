d3ebc2fc1f7bd79f92bfb02e5a1a21d51faa05a3
lunov08 <lukitmate@gmail.com>
2021-11-15 10:07:15 -0300

    add rooms route

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const path = require('path')
 
+const mongoose = require('mongoose')
+const Room = require('./models/room')
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/room-rent');
+}
 
 const app = express()
 
@@ -10,6 +18,11 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/rooms', async (req, res) => {
+    const rooms = await Room.find({})
+    res.render('index', { rooms })
 })
 
 
