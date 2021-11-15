5732cd8d2eba7a76fb1d98e662ccfbb369ff5d4c
lunov08 <lukitmate@gmail.com>
2021-11-15 10:47:44 -0300

    add route to create a room

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ async function main() {
 
 const app = express()
 
+// EJS CONFIG
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// BODY PARSER
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -25,11 +28,23 @@ app.get('/rooms', async (req, res) => {
     res.render('rooms/index', { rooms })
 })
 
+app.get('/rooms/new', (req, res) => {
+    res.render('rooms/new')
+})
+
+app.post('/rooms', async (req, res) => {
+    const room = new Room(req.body.room);
+    await room.save()
+    res.redirect(`/rooms/${room._id}`)
+})
+
 app.get('/rooms/:id', async (req, res) => {
     const { id } = req.params
     const room = await Room.findById(id)
     res.render('rooms/show', { room })
 })
+
+
 
 
 app.listen(3000, (req, res) => {
