b11acba878009fecff0e846357730b0529b8fa28
lunov08 <lukitmate@gmail.com>
2021-11-16 19:05:46 -0300

    config session

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const engine = require('ejs-mate')
+const session = require('express-session')
+
 
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
@@ -32,6 +34,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'thisismysecret',
+    resave: false,
+    saveUnitialized: true,
+    cookie: {
+        httpOnly : true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 
 app.use('/rooms', rooms)
