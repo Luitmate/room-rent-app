757e19460cbf291a3a06a58c2a60aced45155871
lunov08 <lukitmate@gmail.com>
2021-11-18 19:54:01 -0300

    config reviews controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,25 +6,12 @@ const catchAsync = require('../utils/catchAsync')
 const Room = require('../models/room')
 const Review = require('../models/review')
 
+const reviews = require('../controllers/reviews')
+
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware')
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async(req, res) => {
-    const room = await Room.findById(req.params.id)
-    const review = new Review(req.body.review)
-    review.author = req.user._id
-    room.reviews.push(review)
-    await review.save()
-    await room.save()
-    req.flash('success', 'Created new review!')
-    res.redirect(`/rooms/${room._id}`)
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async(req, res) => {
-    const { id, reviewId } = req.params
-    await Room.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Room.findByIdAndDelete(reviewId)
-    req.flash('success', 'Successfully deleted review')
-    res.redirect(`/rooms/${id}`)
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router
