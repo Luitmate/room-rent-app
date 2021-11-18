eab081a9003efc28d08c324967375a0bce215c7b
lunov08 <lukitmate@gmail.com>
2021-11-18 19:52:46 -0300

    config rooms controller

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,65 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
+const rooms = require('../controllers/rooms')
+
 const {isLoggedIn, validateRoom, isAuthor} = require('../middleware')
 
 const catchAsync = require('../utils/catchAsync')
 const Room = require('../models/room')
 
 
-router.get('/', catchAsync(async (req, res) => {
-    const rooms = await Room.find({})
-    res.render('rooms/index', { rooms })
-}))
+router.get('/', catchAsync(rooms.index))
 
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('rooms/new')
-})
+router.get('/new', isLoggedIn, rooms.renderNewForm)
 
-router.post('/', isLoggedIn, validateRoom, catchAsync(async (req, res) => {
-    const room = new Room(req.body.room);
-    room.author = req.user._id
-    await room.save()
-    res.redirect(`/rooms/${room._id}`)
-}))
+router.post('/', isLoggedIn, validateRoom, catchAsync(rooms.createRoom))
 
-router.get('/:id', catchAsync(async (req, res) => {
-    const { id } = req.params
-    const room = await Room.findById(id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author')
-    if(!room) {
-        req.flash('error', 'Cannot find that room!')
-        return res.redirect('/rooms')
-    }
-    res.render('rooms/show', { room })
-}))
+router.get('/:id', catchAsync(rooms.showRoom))
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params
-    const room = await Room.findById(id)
-    if(!room) {
-        req.flash('error', 'Cannot find that room!')
-        return res.redirect('/rooms')
-    }
-    res.render('rooms/edit', { room })
-}))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(rooms.renderEditForm))
 
-router.put('/:id', isLoggedIn, validateRoom, catchAsync(async (req, res) => {
-    const { id } = req.params
-    const room = await Room.findByIdAndUpdate(id, {... req.body.room })
-    req.flash('success', 'Successfully updated room')
-    res.redirect(`/rooms/${room._id}`)
-}))
+router.put('/:id', isLoggedIn, validateRoom, catchAsync(rooms.updateRoom))
 
-router.delete('/:id', isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params
-    await Room.findByIdAndDelete(id)
-    req.flash('success', 'Successfully deleted room')
-    res.redirect('/rooms')
-}))
+router.delete('/:id', isAuthor, catchAsync(rooms.deleteRoom))
 
 module.exports = router
