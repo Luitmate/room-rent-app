670ae4412eb95c015a5e699781420df5a65abdfb
lunov08 <lukitmate@gmail.com>
2021-11-18 19:56:39 -0300

    config users controller

--- routes/users.js
+++ routes/users.js
@@ -4,43 +4,16 @@ const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 const passport = require('passport')
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+const users = require('../controllers/users')
 
-router.post("/register", catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, err => {
-        if(err) return next(err)
-        req.flash("success", "Welcome");
-        res.redirect('/rooms')
-      })
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/register");
-    }
-  })
-);
+router.get("/register", users.renderRegister);
 
-router.get('/login', (req,res) => {
-    res.render('users/login')
-})
+router.post("/register", catchAsync(users.registerUser));
 
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-    req.flash('success', 'Welcome back!')
-    const redirectUrl = req.session.returnTo || '/rooms'
-    delete req.session.returnTo
-    res.redirect(redirectUrl)
-})
+router.get('/login', users.renderLogin)
 
-router.get('/logout', (req, res) => {
-    req.logout()
-    req.flash('success', 'Thank to visit us')
-    res.redirect('/rooms')
-})
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
 
+router.get('/logout', users.logout)
 
 module.exports = router;
