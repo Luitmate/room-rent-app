d042b425194865689368adb6258f3d093099cb84
lunov08 <lukitmate@gmail.com>
2021-11-24 00:38:10 -0300

    add maps control

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -6,6 +6,8 @@ mapboxgl.accessToken = mapToken;
   zoom: 9 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
 new mapboxgl.Marker()
     .setLngLat(room.geometry.coordinates)
     .setPopup(
